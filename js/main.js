cbfcc5ccffb9f2f2a17a5e96442b43d6cb7d1980
BestGak <pro100gall@yandex.ru>
2021-10-10 15:03:06 +0300

    New hometask

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-const studentsGirl = ["Олена","Іра", "Світлана"]
-const studetsMan = ["Олександр", "Ігор", "Олексій"]
-// const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
-const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = [
+  "Диференційне рівняння",
+  "Теорія автоматів",
+  "Алгоритми і структури даних",
+];
 const marks = [4, 5, 5, 3, 4, 5];
 
-const pares = () => {
-let students = [];
-for(i = 0; i < studentsGirl.length; i++) {
-  students.push(studentsGirl[i].concat(studetsMan[i]).split(' ')) ;
-}
-students.join('')
-return console.log(students);
-}
+const studentsArr = (arrWithStudents) => {
+  const pares = [];
+  const girls = arrWithStudents.filter((student) => student[student.length - 1] == "а");
+  const mans = arrWithStudents.filter((student) => student[student.length - 1] !== "а");
+  for (let i = 0; i < girls.length; i++) {
+    pares.push([girls[i], mans[i], themes[i]]);
+  }
+  return pares;
+};
 
-pares();
+const marksStudents = (arrStudents, arrMarks) => {
+  const arrWithMarks = [];
+  for (let i = 0; i < marks.length; i++) {
+    arrWithMarks.push([arrMarks[i], arrStudents[i]]);
+  }
+  return arrWithMarks;
+};
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const randomMarks = (studentsArr, arrThemes) => {
+  const arrWithAllInforamtion = [];
+  const girls = studentsArr.filter((student) => student[student.length - 1] == "а");
+  const mans = studentsArr.filter((student) => student[student.length - 1] !== "а");
+  for (let i = 0; i < girls.length; i++) {
+    arrWithAllInforamtion.push([
+      mans[i] + " i " + girls[i],
+      arrThemes[i],
+      getRandomInt(5),
+    ]);
+  }
+  return arrWithAllInforamtion;
+};
+
+console.log(studentsArr(students));
+console.log(marksStudents(students, marks));
+console.log(randomMarks(students, themes));
