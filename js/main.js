60cdc64105ff2ae1c38f0b2aca4ecbb0ea9ea6c0
BestGak <pro100gall@yandex.ru>
2021-10-15 12:39:19 +0300

    Refactoring hometask Arrays

--- js/main.js
+++ js/main.js
@@ -5,40 +5,58 @@ const themes = [
   "Алгоритми і структури даних",
 ];
 const marks = [4, 5, 5, 3, 4, 5];
+const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const studentsArr = (arrWithStudents) => {
+  const girls = [];
+  const men = [];
   const pares = [];
-  const girls = arrWithStudents.filter((student) => student[student.length - 1] == "а");
-  const mans = arrWithStudents.filter((student) => student[student.length - 1] !== "а");
-  for (let i = 0; i < girls.length; i++) {
-    pares.push([girls[i], mans[i], themes[i]]);
+
+  for (const student of arrWithStudents) {
+    if (student[student.length - 1] !== "а") {
+      men.push(student);
+    } else {
+      girls.push(student);
+    }
   }
+
+  for (let i = 0; i < girls.length; i++) {
+    pares.push([girls[i], men[i]]);
+  }
+
   return pares;
 };
+
+const arrPares = studentsArr(students);
+
+const paresTheme = (pares, theme) => {
+  let paresWithTheme = [];
+  for (let i = 0; i < theme.length; i++) {
+    paresWithTheme.push([pares[i].join(" i "), theme[i]]);
+  }
+  return paresWithTheme;
+};
+
+const arrParesWithTheme = paresTheme(arrPares, themes);
 
 const marksStudents = (arrStudents, arrMarks) => {
   const arrWithMarks = [];
   for (let i = 0; i < marks.length; i++) {
-    arrWithMarks.push([arrMarks[i], arrStudents[i]]);
+    arrWithMarks.push([arrStudents[i], arrMarks[i]]);
   }
   return arrWithMarks;
 };
-const getRandomInt = (max) => Math.floor(Math.random() * max);
 
-const randomMarks = (studentsArr, arrThemes) => {
-  const arrWithAllInforamtion = [];
-  const girls = studentsArr.filter((student) => student[student.length - 1] == "а");
-  const mans = studentsArr.filter((student) => student[student.length - 1] !== "а");
-  for (let i = 0; i < girls.length; i++) {
-    arrWithAllInforamtion.push([
-      mans[i] + " i " + girls[i],
-      arrThemes[i],
-      getRandomInt(5),
-    ]);
+const randomMarks = (arrWithStudentsAndTheme) => {
+  let arrWithAllInforamtion = [];
+  for (let i = 0; i < arrWithStudentsAndTheme.length; i++) {
+    arrWithAllInforamtion.push(arrWithStudentsAndTheme[i].concat(getRandomInt(5)));
+    // debugger
   }
   return arrWithAllInforamtion;
 };
 
 console.log(studentsArr(students));
+console.log(paresTheme(arrPares, themes));
 console.log(marksStudents(students, marks));
-console.log(randomMarks(students, themes));
+console.log(randomMarks(arrParesWithTheme));
