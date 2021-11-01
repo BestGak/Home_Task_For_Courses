2994cfa2ce0c146e78f74fa392f4134c8fec2c8e
BestGak <pro100gall@yandex.ru>
2021-11-01 23:47:24 +0200

    Add new hometask THIS

---
+++ THIS-HW7/main.js
@@ -0,0 +1,33 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+function getMyTaxes(salary) {
+  return this.tax * salary;  
+}
+
+function getMiddleTaxes(country) {
+  return this.tax * this.middleSalary
+}
+
+function getTotalTaxes(country) {
+  return this.tax * this.middleSalary * this.vacancies
+}
+
+function getMySalary(country) {
+const randomSalary = Math.floor(Math.random() * (15000 - 10000) + 10000)
+const taxes = Math.floor(getMyTaxes.call(country , randomSalary));
+const profit = Math.floor(randomSalary - taxes)
+const salary = {
+  randomSalary,
+  taxes,
+  profit
+}
+return salary;
+}
+
+
+console.log( `Налог с зарплаты в 10000грн: ${getMyTaxes.call(ukraine, 10000)}грн`);
+console.log(`Столько в среднем платят налогоплательщики Украины ${getMiddleTaxes.call(ukraine)}грн`);
+console.log(`Столько всего налогов платят специалисты-IT ${getTotalTaxes.call(ukraine)}`);
+console.log(getMySalary(ukraine));
